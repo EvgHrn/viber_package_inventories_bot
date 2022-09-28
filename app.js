1d23ab1e72e12bcf089e4fc80b7c7f355770309a
EvgHrn <evg.hrn@gmail.com>
2022-09-28 15:35:27 +0400

    Echo to admin

--- app.js
+++ app.js
@@ -3,24 +3,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-require('dotenv').config();
-
-const ViberBot = require('viber-bot').Bot;
-const BotEvents = require('viber-bot').Events;
-
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-
-const bot = new ViberBot({
-    authToken: process.env.TOKEN,
-    name: "Описи",
-    avatar: "http://viber.com/avatar.jpg" // It is recommended to be 720x720, and no more than 100kb.
-});
-
-bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
-    // Echo's back the message to the client. Your bot logic should sit here.
-    response.send(message);
-});
 
 const app = express();
 
